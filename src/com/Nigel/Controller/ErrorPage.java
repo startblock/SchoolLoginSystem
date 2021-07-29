29a8f89a8ed12b304c3aaf8fd2fd1284e44ed69c
startblock <burkenigel1995@gmail.com>
2021-07-29 19:40:15 +0100

    Added error window functionality

---
+++ src/com/Nigel/Controller/ErrorPage.java
@@ -0,0 +1,11 @@
+package com.Nigel.Controller;
+
+import com.Nigel.View.PageFactory;
+
+public class ErrorPage extends BaseController {
+
+    public ErrorPage(PageFactory pageFactory, String fxmlString) {
+        super(pageFactory, fxmlString);
+    }
+
+}
